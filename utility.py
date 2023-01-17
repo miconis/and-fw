4c0a91d84817c7601e9891b184ac7e41a9ec553d
miconis <michele.debonis@isti.cnr.it>
2023-01-17 17:11:39 +0100

    gitignore updated

--- utility.py
+++ utility.py
@@ -1,11 +1,14 @@
 import hashlib
 import json
 
+
 def test(x):
     print(x)
+
 
 def list_to_file(dict_list, file_path):
     with open(file_path, 'a') as file:
         file.truncate(0)
         for elem in dict_list:
             json.dump(elem, file)
+            file.write("\n")
